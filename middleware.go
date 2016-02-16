698507c63fea43aef9cb8d58e876be0d17357508
Miek Gieben <miek@miek.nl>
2016-02-16 15:28:32 +0100

    more

--- middleware.go
+++ middleware.go
@@ -7,23 +7,18 @@ import (
 	"github.com/mholt/caddy/middleware"
 )
 
-const nlgids = "nlgids.london"
-
 func Setup(c *setup.Controller) (middleware.Middleware, error) {
 	return func(next middleware.Handler) middleware.Handler {
 		return &handler{Next: next}
 	}, nil
 }
 
-type handler struct {
-	Next middleware.Handler
-}
+type handler struct{ Next middleware.Handler }
 
 func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) (int, error) {
 	if r.Method != "POST" {
 		return h.Next.ServeHTTP(w, r)
 	}
-
 	path := middleware.Path(r.URL.Path)
 	if !path.Matches("/api") {
 		return h.Next.ServeHTTP(w, r)
@@ -33,11 +28,11 @@ func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) (int, error) 
 	case path.Matches("/api/auth/test"):
 		return WebInvoiceTest(w, r)
 	case path.Matches("/api/auth/invoice"):
-
+		//		return WebInvoice(w, r)
 	case path.Matches("/api/open/contact"):
 		return WebContact(w, r)
 	case path.Matches("/api/open/booking"):
-
+		//		return WebBooking(w, r)
 	case path.Matches("/api/open/calendar"):
 		return WebCalendar(w, r)
 	}
