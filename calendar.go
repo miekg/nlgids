34c919856fdc30539cf7f66e00ecb3ac652510ad
Miek Gieben <miek@miek.nl>
2019-06-19 06:56:34 +0100

    Add more options here
    Signed-off-by: Miek Gieben <miek@miek.nl>

--- calendar.go
+++ calendar.go
@@ -8,9 +8,12 @@ import (
 )
 
 // WebCalendar returns a calendar in table form. All-day events from the
-// subject are greyed out,  as are pasted days.
+// subject are greyed out, as are pasted days.
 func (n *NLgids) WebCalendar(w http.ResponseWriter, r *http.Request) (int, error) {
-	date := r.PostFormValue("date") // YYYY-MM-DD, empty is allowed.
+	date := r.PostFormValue("date")     // YYYY-MM-DD, empty is allowed
+	tour := r.PostFormValue("tourtype") // see tours.json in the site, this is the "type"
+	tour := r.PostFormValue("tourid")   // see tours.json in the site, this is the "id"
+
 	c, err := calendar.New(date, n.Config.Subject, n.Config.Secret)
 	if err != nil {
 		return http.StatusBadRequest, nil
